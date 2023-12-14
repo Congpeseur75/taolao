4d5d930b226802b63032f738f71d6f9901c56ba9
Congpeseur75 <cn247123@gmail.com>
2023-12-14 13:48:48 +0700

    len

---
+++ index.js
@@ -0,0 +1,8 @@
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML = 'xin chào';}
+function showMessage(){
+    console.log('Xin chào!');
+}
+function showMessage(){
+    document.write('Xin chào!');
+}
